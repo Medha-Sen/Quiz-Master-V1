85377563112ebb829b9bf8280639027e3717290d
medha sen <medhasen1001@gmail.com>
2025-02-04 23:46:43 +0530

    Created database schema for users, subjects, quizzes, and scores

--- quiz_master_21f1000057/models/models.py
+++ quiz_master_21f1000057/models/models.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'iitm_quiz_master_v1.db')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///iitm_quiz_master_v1.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db=SQLAlchemy(app)
